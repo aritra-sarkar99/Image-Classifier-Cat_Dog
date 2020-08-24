cbcc2520f6cb81c48c39d3443d441378664e84cc
aritra-sarkar99 <aritras.ece2018@nsec.ac.in>
2020-08-24 15:47:24 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask,redirect,request,url_for
+from flask import render_template as ren
+import os
+from werkzeug.utils import secure_filename
+import uuid
+
+app = Flask(__name__)
+
+UPLOAD_PATH = 'uploads'
+app.config['UPLOAD_PATH'] = UPLOAD_PATH
+
+def model(filepath):
+    return 1 or 0
+@app.route("/")
+def home():
+    return ren("index.html")
+
+# @app.route("/img-upload", methods=['GET','POST'])
+# def upload():
+    
+#     if request.method == 'POST':
+
+#         if request.files:
+
+#             image = request.files['image']
+#             print(image)
+#             id = uuid.uuid1()
+
+#             if secure_filename(image.filename):
+#                 filename = image.filename
+#                 ext =  filename.rsplit(".",1)[1]
+#                 filename = id.hex + "." + ext  ######### FileName of uploaded file ############
+#                 basepath = os.path.dirname(__file__)
+#                 file_path = os.path.join(basepath,'templates','uploads',secure_filename(filename))
+#                 print(file_path)
+#                 image.save(file_path)
+#                 return redirect(request.url)
+
+#     return ren("index.html")
+
+@app.route('/upload-new',methods=['GET','POST'])
+def upload_file():
+     if request.method == 'POST':
+
+        
+        if request.files:
+
+            image = request.files['image']
+            id = uuid.uuid1()
+            print(image)
+            if image:
+
+                filename = image.filename
+                # ext =  filename.rsplit(".",1)[1]
+                # filename = id.hex + "." + ext  ######### FileName of uploaded file ############
+                filename = secure_filename(image.filename)
+
+                file_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+                image.save(file_path)
+                # return send_from_directory(app.config['UPLOAD_PATH'],
+                #                filename)
+                label = model(file_path) #Call your model here and return a string or binary
+                name = ''
+                if label==1:
+                    name = 'Dog'
+                else:
+                    name = 'Cat'
+                os.remove(file_path)
+                return ren("index.html",label=name)
+
+# from flask import send_from_directory
+# from werkzeug.middleware.shared_data import SharedDataMiddleware
+
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_PATH'],
+#                                filename)
+
+
+# app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+#                  build_only=True)
+# app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+#     '/uploads':  app.config['UPLOAD_PATH']
+# })
+
+if __name__ == '__main__':
+     app.run(debug=True)
